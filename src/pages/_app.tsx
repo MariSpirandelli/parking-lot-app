09ad8185b4f9f5de710a179cbbd742c505d4f388
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-02-15 20:08:05 -0300

    next configuration pages update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+import { ThemeProvider } from '@mui/material';
+import '../styles/colors.css';
+import { materialUITheme } from '../styles/materialUI';
+import { ParkingLotProvider } from '@/context/parkingLot';
+import { GlobalStyle } from '@/styles/global';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={materialUITheme}>
+        <ParkingLotProvider {...pageProps}>
+          <Head>
+            <title>Parking Lot</title>
+            <meta name="description" content="Parking lot app" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <div className="content-bg">
+            <Component {...pageProps} />
+          </div>
+        </ParkingLotProvider>
+      </ThemeProvider>
+    </>
+  );
 }
