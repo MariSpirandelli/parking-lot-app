ba8103d2c5e0122b8484f14b6ef9f37e1d7bb030
Mari Spirandelli <mariannaspirandelli@gmail.com>
2023-02-16 09:44:52 -0300

    vehicle service integration

---
+++ src/helpers/api/vehicle.ts
@@ -0,0 +1,20 @@
+import api from './index';
+
+const save = (
+  plate: string,
+  vehicleTypeId: number
+): Promise<IVehicle> => {
+  const body = {
+    plate,
+    vehicleTypeId
+  };
+
+  return api.post(`/api/vehicles/`, {
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  });
+};
+
+export default {
+  save,
+};
